0ddd2869048400a7e6293caa7c432f59c080884d
Ian <ianrodrigues2001@hotmail.com>
2024-03-12 16:35:25 -0300

    Adicionando GET por id no AlunoController

--- java-avancado/aula-01/db_crud/src/main/java/org/db_crud/db_crud/controller/AlunoController.java
+++ java-avancado/aula-01/db_crud/src/main/java/org/db_crud/db_crud/controller/AlunoController.java
@@ -4,7 +4,9 @@ import org.db_crud.db_crud.controller.dto.AlunoDTO;
 import org.db_crud.db_crud.model.Aluno;
 import org.db_crud.db_crud.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Aluno/")
+@RequestMapping("/aluno/")
 public class AlunoController {
 
     @Autowired
@@ -31,6 +33,28 @@ public class AlunoController {
         }
 
         return listaAlunoDTO;
+    }
+
+    public ResponseEntity<?> listaAlunosId(@PathVariable Integer id){
+
+        if (id == null){
+
+            return ResponseEntity.badRequest().build();
+
+        }
+
+        try {
+
+            Aluno aluno = alunoRepository.getReferenceById(id);
+            AlunoDTO alunoDTO = new AlunoDTO(aluno);
+            return ResponseEntity.ok(alunoDTO);
+
+        } catch (Exception e){
+
+            return ResponseEntity.notFound().build();
+
+        }
+
     }
 
 }
