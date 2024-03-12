508537dc5bef32e328aadda084b2e091a495b63a
Ian <ianrodrigues2001@hotmail.com>
2024-03-12 16:30:27 -0300

    Modificando application.properties para que as tabelas sejam criadas no postgres e adicionando o GET no AlunoController

--- java-avancado/aula-01/db_crud/src/main/java/org/db_crud/db_crud/controller/AlunoController.java
+++ java-avancado/aula-01/db_crud/src/main/java/org/db_crud/db_crud/controller/AlunoController.java
@@ -1,4 +1,36 @@
 package org.db_crud.db_crud.controller;
 
+import org.db_crud.db_crud.controller.dto.AlunoDTO;
+import org.db_crud.db_crud.model.Aluno;
+import org.db_crud.db_crud.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/Aluno/")
 public class AlunoController {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @GetMapping
+    public List<AlunoDTO> retornaAlunos(String nome){
+
+        List<Aluno> listaAluno = (ArrayList<Aluno>) alunoRepository.findAll();
+        List<AlunoDTO> listaAlunoDTO = new ArrayList<AlunoDTO>();
+        for (Aluno aluno : listaAluno){
+
+            AlunoDTO alunoDTO = new AlunoDTO(aluno);
+            listaAlunoDTO.add(alunoDTO);
+
+        }
+
+        return listaAlunoDTO;
+    }
+
 }
