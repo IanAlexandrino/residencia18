06355ab82d74c7e1953bb083efb397e70c5e40d8
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-25 18:28:18 -0300

    Adicionando novo model Escola

---
+++ java-avancado/aula-02/db_crud/src/main/java/org/db_crud/db_crud/model/Escola.java
@@ -0,0 +1,48 @@
+package org.db_crud.db_crud.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Escola {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer id;
+    public String nome;
+    public String localizacao;
+
+    public Escola() {
+    }
+
+    public Escola(Integer id, String nome, String localizacao) {
+        this.id = id;
+        this.nome = nome;
+        this.localizacao = localizacao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+}
