9311ff3f67c9e2b50d3365215e166840dcfc0fb6
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-03-25 18:47:34 -0300

    Retirando método obsoleto do AlunoRepository e adicionando EscolaRepository

---
+++ java-avancado/aula-02/db_crud/src/main/java/org/db_crud/db_crud/repository/EscolaRepository.java
@@ -0,0 +1,7 @@
+package org.db_crud.db_crud.repository;
+
+import org.db_crud.db_crud.model.Escola;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EscolaRepository extends JpaRepository<Escola, Integer> {
+}
