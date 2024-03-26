ecc1050d76247290bcbc51d616ce023f0c70e5a6
Ian <ianrodrigues2001@hotmail.com>
2024-03-26 14:52:47 -0300

    Adicionando anotações do lombok para a model Employee

--- java-avancado/aula-06/controle_empregado_projeto/src/main/java/org/controle_empregado_projeto/controle_empregado_projeto/model/Employee.java
+++ java-avancado/aula-06/controle_empregado_projeto/src/main/java/org/controle_empregado_projeto/controle_empregado_projeto/model/Employee.java
@@ -1,61 +1,45 @@
 package org.controle_empregado_projeto.controle_empregado_projeto.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
-import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "EMPLOYEE")
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
+    @Getter
+    @Setter
     private int id;
 
     @Column(name = "name")
+    @Getter
+    @Setter
     private String name;
 
     @Column(name = "email")
+    @Getter
+    @Setter
     private String email;
 
     @Column(name = "technicalSkill")
+    @Getter
+    @Setter
     private String technicalSkill;
 
     @ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH })
     @JoinTable(name = "EMPLOYEE_PROJECT_MAPPING", joinColumns = @JoinColumn(name = "employee_id"),
             inverseJoinColumns = @JoinColumn(name = "project_id"))
+    @Getter
+    @Setter
     private Set<Project> projects;
-
-    public Employee() {
-    }
-
-    public Employee(String name, String email, String technicalSkill) {
-        this.name = name;
-        this.email = email;
-        this.technicalSkill = technicalSkill;
-    }
-
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public String getTechnicalSkill() { return technicalSkill; }
-    public void setTechnicalSkill(String technicalSkill) { this.technicalSkill = technicalSkill; }
-
-    public Set<Project> getProjects() { return projects; }
-    public void setProjects(Set<Project> projects) { this.projects = projects; }
-
-    @Override
-    public String toString() {
-        return "Employee [email=" + email + ", id=" + id + ", name=" + name
-                + ", technicalSkill=" + technicalSkill + "]";
-    }
 
 }
