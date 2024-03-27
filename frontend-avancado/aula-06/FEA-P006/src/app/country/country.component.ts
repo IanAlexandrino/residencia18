5927e49e70b8221bac45106eff1a716ecc508e72
Ian <ianrodrigues2001@hotmail.com>
2024-03-27 16:23:56 -0300

    Criando projeto e iniciando desenvolvimento da FEA-P006

---
+++ frontend-avancado/aula-06/FEA-P006/src/app/country/country.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+
+@Component({
+  selector: 'app-country',
+  standalone: true,
+  imports: [],
+  templateUrl: './country.component.html',
+  styleUrl: './country.component.css'
+})
+export class CountryComponent implements OnInit {
+
+  country: any; // Para armazenar o primeiro país
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.fetchCountryData();
+  }
+
+  async fetchCountryData() {
+    try {
+      const response = await axios.get('https://restcountries.com/v3.1/all');
+      // Pegar o primeiro país da resposta
+      this.country = response.data[0];
+      // Transformar os dados para o formato desejado (se necessário)
+      this.transformCountryData();
+    } catch (error) {
+      console.error('Erro ao buscar dados do país:', error);
+    }
+  }
+
+  transformCountryData() {
+    // Aqui você pode transformar os dados do país conforme necessário
+    // Por exemplo, reorganizar propriedades, formatar valores, etc.
+    // Para este exemplo, vamos apenas exibir o nome do país no console
+    console.log('Nome do país:', this.country.name);
+  }
+
+}
