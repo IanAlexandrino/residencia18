717493a36d9ffd6ce4758b7faae93cd8494276be
Ian <ianrodrigues2001@hotmail.com>
2024-04-04 10:22:06 -0300

    Adicionando projeto pratico pra exercício da aula_08

---
+++ frontend-avancado/aula-08/exercicio_01/src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { Componente1Component } from './menu/componente-1/componente-1.component';
+import { Componente2Component } from './menu/componente-2/componente-2.component';
+import { Componente3Component } from './menu/componente-3/componente-3.component';
+
+
+export const routes: Routes = [
+    {
+        path: 'menu-componente',
+        component: MenuComponent
+    },
+    {
+
+    }
+];
