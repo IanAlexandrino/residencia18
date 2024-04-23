2a11bba1ccac5572ede85054df23dff2f33b9f3c
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-04-22 23:33:03 -0300

    Implementando dados com javafaker no teste da model Escola

--- java-avancado/aula-08/db_crud/src/test/java/org/db_crud/db_crud/models/EscolaTest.java
+++ java-avancado/aula-08/db_crud/src/test/java/org/db_crud/db_crud/models/EscolaTest.java
@@ -1,28 +1,41 @@
 package org.db_crud.db_crud.models;
 
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EscolaTest {
 
+    Escola escola1;
+    Escola escola2;
+    Escola escola3;
+
+    static Faker faker = new Faker();
+
+    public class ModelMock{
+        public static final int idEscola = faker.number().randomDigit();
+        public static final String nomeEscola = faker.company().name();
+        public static final String endereco = faker.address().streetAddress();
+    }
+
+    @BeforeEach
+    public void setUp(){
+        escola1 = new Escola(ModelMock.idEscola, ModelMock.nomeEscola, ModelMock.endereco);
+        escola2 = new Escola(escola1.getId(), escola1.getNome(), escola1.getLocalizacao());
+        escola3 = new Escola(faker.number().randomDigit(), faker.company().name(), faker.address().streetAddress());
+    }
+
     @Test
     public void testaConstrutoresEGetters(){
-
-        Escola escola = new Escola(1, "Escola Teste", "Localização Teste");
-
-        assertEquals(1, escola.getId());
-        assertEquals("Escola Teste", escola.getNome());
-        assertEquals("Localização Teste", escola.getLocalizacao());
+        assertEquals(ModelMock.idEscola, escola1.getId());
+        assertEquals(ModelMock.nomeEscola, escola1.getNome());
+        assertEquals(ModelMock.endereco, escola1.getLocalizacao());
     }
 
     @Test
     public void testaEqualsEHashCode() {
-
-        Escola escola1 = new Escola(1, "Escola Teste", "Localização Teste");
-        Escola escola2 = new Escola(1, "Escola Teste", "Localização Teste");
-        Escola escola3 = new Escola(2, "Outra Escola", "Outra Localização");
-
         assertEquals(escola1, escola2);
         assertEquals(escola1.hashCode(), escola2.hashCode());
         assertNotEquals(escola1, escola3);
