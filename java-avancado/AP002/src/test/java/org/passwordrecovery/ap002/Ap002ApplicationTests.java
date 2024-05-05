66b490347700f74d54a270af10d23cf1cc779d07
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-05-04 23:21:08 -0300

    Adicionando projeto base para resolução da prova

---
+++ java-avancado/AP002/src/test/java/org/passwordrecovery/ap002/Ap002ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.passwordrecovery.ap002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ap002ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
